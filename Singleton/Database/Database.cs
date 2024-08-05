a0a60c7aed54b21ace79b66e6a6b93cb19d98678
GonzaloSilvera21 <gsilveradesarrollos@gmail.com>
2024-08-05 11:41:52 -0300

    Singleton

---
+++ Singleton/Database/Database.cs
@@ -0,0 +1,22 @@
+﻿namespace Singleton.Database
+{
+    public class Database
+    {
+        private static Database _database;
+        private static object _lock = new object();
+        
+        private Database() { }
+
+        public static Database GetDatabase()
+        {
+            if(_database == null)
+            {
+                lock(_lock) {
+                    if(_database == null) { _database = new Database(); }
+                }
+            }
+
+            return _database;
+        }
+    }
+}
