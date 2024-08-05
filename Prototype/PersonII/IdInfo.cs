190fa90aa2188079d410b5282837f46097fc2d15
GonzaloSilvera21 <gsilveradesarrollos@gmail.com>
2024-08-05 11:25:21 -0300

    Prototype v2

---
+++ Prototype/PersonII/IdInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Prototype.PersonII
+{
+    public class IdInfo
+    {
+        public int IdNumber;
+        public IdInfo(int idNumber) => IdNumber = idNumber;
+    }
+}
---
+++ Prototype/PersonII/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace Prototype.PersonII
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime Birthday;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowPerson() => (Person)MemberwiseClone();
+
+        [Obsolete]
+        public Person DeepCopy()
+        {
+            Person clone = (Person)MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = string.Copy(Name);
+            return clone;
+        }
+    }
+}
